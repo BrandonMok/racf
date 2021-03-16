e7a06c0b32307b392d4a2e331fa8b5514a3a691b
Brandon <bxm5989@rit.edu>
2021-03-16 19:04:32 -0400

    converted ids to class

--- modules/custom/qr_code/js/onclick.js
+++ modules/custom/qr_code/js/onclick.js
@@ -2,7 +2,7 @@
     Drupal.behaviors.myBehavior = {
         attach: function(context, settings) {
             $("#access-pass").once('myBehavior').click(function() {
-                $("#access-pass__content").fadeIn("slow");
+                $(".access-pass__content").fadeIn("slow");
             });
 
             // @TODO for analytics - do another .click and need to make an AJAX call to update bc in JS.

ba7efe2fc8e8d0ea13a783259d2b6ea26bd86528
Brandon <bxm5989@rit.edu>
2021-03-25 20:18:25 -0400

    Stubbed in working code to get event data

--- modules/custom/analytics/src/Controller/AnalyticsController.php
+++ modules/custom/analytics/src/Controller/AnalyticsController.php
@@ -11,6 +11,57 @@ class AnalyticsController extends ControllerBase {
 
         $currentUID = \Drupal::currentUser()->id();    // current user's ID
 
+        $entityTypeManager = \Drupal::entityTypeManager();
+        $events = $entityTypeManager->getStorage('node')->loadByProperties([
+            'type' => 'event', 
+            'uid' => $currentUID
+        ]);
+
+        $generalEvents = $entityTypeManager->getStorage('node')->loadByProperties([
+            'type' => 'general_event', 
+            'uid' => $currentUID
+        ]);
+
+        /**
+         * Both $events and $generalEvents are arrays of nodes
+         * Would do a foreach through all to get the specific info
+         */
+        // Uncomment the two below lines if you want / need to see the structure of the nodes
+        // dump($events);
+        // dump($generalEvents);
+
+        // Note: VSCODE may show red squiggly line under "->get". Just ignore it bc it's a php thing apparently, it works so don't worry about it
+        foreach($events as $e) {
+            $genPasses = $e->get('field_generated_passes')->getValue();
+            $genPasses = $genPasses[0]; // the actual valued number
+
+            $scannedPasses = $e->get('field_scanned_passes')->getValue();
+            $scannedPasses = $scannedPasses[0];
+
+            /**
+             * TODO:
+             * Do something with the number of generated and scanned passes
+             */
+        }
+
+        /**
+         * FINISH THIS BELOW
+         */
+        // foreach($generalEvents as $genEvent) {
+        //     // do the samething for the above for regular events. (General Events has the same 'field_generated_passes' and 'field_scanned_passes' fields)
+
+        // }
+     
+
+
+
+        
+
+
+
+        /**
+         * Leaving this here just in case, but don't think we'd need it
+         */
         //$value = field_extract_value('node', $node, 'field_just_a_value');
         //$entity->get('field_name')->getValue();
 
@@ -21,47 +72,26 @@ class AnalyticsController extends ControllerBase {
       //]);
       //
       
-        $events = $entityTypeManager->getStorage('node')->loadByProperties([
-            'type' => 'event', 
-            'author' => $currentUID
-        ]);
-        dump($events)
+        // $result = $query
+        //     ->entityCondition('entity_type', 'node')
+        //     ->propertyCondition('uid', $currentUID)
+        //     ->execute();
 
-
-        $result = $query
-            ->entityCondition('entity_type', 'node')
-            ->propertyCondition('uid', $currentUID)
-            ->execute();
-
-        // Get the definitions
-        $definitions = \Drupal::service('entity_field.manager')->getFieldDefinitions('node', 'my_custom_content');
+        // // Get the definitions
+        // $definitions = \Drupal::service('entity_field.manager')->getFieldDefinitions('node', 'my_custom_content');
 
         // Load a node for which you want to get the field values
-        $my_node = \Drupal\node\Entity\Node::load($my_nid);
+        // $my_node = \Drupal\node\Entity\Node::load($my_nid);
 
-        // Iterate through the definitions
-        foreach (array_keys($definitions) as $field_name) {
-            // Get the values for your node
-            // Use getValue() if you want to get an array instead of text.
-            $values[$field_name] = $my_node->get($field_name)->value;
-        }
+        // // Iterate through the definitions
+        // foreach (array_keys($definitions) as $field_name) {
+        //     // Get the values for your node
+        //     // Use getValue() if you want to get an array instead of text.
+        //     $values[$field_name] = $my_node->get($field_name)->value;
+        // }
 
-       
-        // If you want this current user obect
-        $entityTypeManager = \Drupal::entityTypeManager();
-        $userStorage = $entityTypeManager->getStorage('user');
 
-        //$userResult = $userStorage->loadByProperties([
-        //    'uid' => $uid
-        //]);
-        //$user = array_pop($userResult);
-
-        $events = $entityTypeManager->getStorage('node')->loadByProperties([
-            'type' => 'event', 
-            //'title' => $event
-        ]);
-
-        $values = \Drupal\Core\Entity\ContentEntityBase::get('field_generated_passes');(edited)
+        // $values = \Drupal\Core\Entity\ContentEntityBase::get('field_generated_passes');
 
 
         return array(

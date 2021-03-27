3b8a212c5c4380437797f41791e340e55c37ee1e
Brandon <bxm5989@rit.edu>
2021-03-27 16:42:06 -0400

    Added time field of event to show on event checkin page

--- modules/custom/qr_code/src/Controller/QRCodeController.php
+++ modules/custom/qr_code/src/Controller/QRCodeController.php
@@ -56,25 +56,27 @@ class QRCodeController extends ControllerBase {
       $today = $today->format('m/d/Y');
 
       $formattedDate = "$start - $to";
+
+      // Event Time 
+      $fullTime = $theEvent->get('field_time')->getString();
+      $startTime = substr($fullTime, 0, 5);
+      $endTime = substr($fullTime, 7, strlen($fullTime));
+
+      $startTimeDate = date('h:m A', $startTime);
+      $endTimeDate = date('h:m A', $endTime);
+
+      $eventTime = "$startTimeDate - $endTimeDate";
     }
     else {
       // not an event - is a General Event
       $formattedDate = "";
     }
 
-
-    // if ($today >= $start && $today <= $to){
-    //   $formattedDate = "$start - $to  (TODAY)";
-    // }
-    // else {
-      // $formattedDate = "$start - $to";
-    // }
-
-
     return [
       '#theme' => 'qr_scan_pass',
       '#event' => $event,
       '#event_date' => $formattedDate,
+      '#event_time' => $eventTime ?? '',
       '#snap' => $snap,
       '#attached' => [
         'library' => [

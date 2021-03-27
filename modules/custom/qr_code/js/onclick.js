a930df8bfbe073ecc6844d2ee029184a23c9fcbf
Brandon <bxm5989@rit.edu>
2021-03-27 16:51:39 -0400

    Removed onclick of button after having opened pass to prevent data skew

--- modules/custom/qr_code/js/onclick.js
+++ modules/custom/qr_code/js/onclick.js
@@ -1,21 +1,18 @@
 (function ($, Drupal) {
     Drupal.behaviors.myBehavior = {
         attach: function(context, settings) {
-            // "Generate Access Pass" button onClick
             $("#access-pass", context).once('myBehavior').click(function() {
                 $(".access-pass__content").fadeIn("slow");
 
-
-
-                // Update passes generated field
                 $eventTitle = $('#event-title').text();
                 $.ajax({
                     type: "POST",
                     url: settings.path.baseUrl + 'passgenerated' + '/' + $eventTitle,
                 });
+
+                $("#access-pass").off('click'); 
             });
 
-            // PRINT btn onClick
             $("#print-text", context).once('myBehavior').click(function() {
                 window.print();
             });

c7a8c9ef81624e3645fe12f46d3d4bb073455b4b
Brandon <bxm5989@rit.edu>
2021-03-29 19:42:08 -0400

    Fixed accessing of each event's generated and scanned passes field value

--- modules/custom/analytics/src/Controller/AnalyticsController.php
+++ modules/custom/analytics/src/Controller/AnalyticsController.php
@@ -45,33 +45,43 @@ class AnalyticsController extends ControllerBase {
         // dump($events);
         // dump($generalEvents);
 
+        $totalGen = 0;
+        $totalScanned = 0;
+
         // Note: VSCODE may show red squiggly line under "->get". Just ignore it bc it's a php thing apparently, it works so don't worry about it
         foreach($events as $e) {
             $genPasses = $e->get('field_generated_passes')->getValue();
-            $genPasses = $genPasses[0];
+            $genPasses = $genPasses[0]["value"];
+            $totalGen = $totalGen + $genPasses;
+
 
             $scannedPasses = $e->get('field_scanned_passes')->getValue();
-            $scannedPasses = $scannedPasses[0];
-
-
-            /**
-             * TODO:
-             * Do something with the number of generated and scanned passes
-             */
+            $scannedPasses = $scannedPasses[0]["value"];
+            $totalScanned = $totalScanned + $scannedPasses;
         }
-
+        
+        
+        
         /**
          * FINISH THIS BELOW
          */
         // foreach($generalEvents as $genEvent) {
-        //     // do the samething for the above for regular events. (General Events has the same 'field_generated_passes' and 'field_scanned_passes' fields)
+            //     // do the samething for the above for regular events. (General Events has the same 'field_generated_passes' and 'field_scanned_passes' fields)
+            
+            // }
+            
+            
+        /**
+         * TODO:
+         * Do something with the number of generated and scanned passes
+         */
 
-        // }
-     
 
 
 
 
+
+         
 
         /**
          * Leaving this here just in case, but don't think we'd need it
